64e2f332d4a369d90d3817f04518c472b7e591c6
PadreCocoRocha <lucasbortoto.lbr@gmail.com>
2017-11-10 16:57:59 -0200

    Refactored request validation as example and changed welcome email

--- Laravel-Learning/first/app/Http/Controllers/RegistrationController.php
+++ Laravel-Learning/first/app/Http/Controllers/RegistrationController.php
@@ -2,39 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistrationRequest;
 use Illuminate\Http\Request;
 use App\Mail\Welcome;
 use App\User;
 
 class RegistrationController extends Controller
 {
-    public function create()
-    {
-    	return view('/registration/create');
-    }
+	public function create()
+	{
+		return view('/registration/create');
+	}
 
-    public function store(){
-    	// Validate Data
-    	$this->validate(request(), [
-    		'name' => 'required',
-    		'email' => 'required|unique:users|email',
-    		'password' => 'required|confirmed'
-    	]);
+	// Injecting the registration request here will make laravel use it's
+	// built-in Request method to validate data (defined in Request::rules())
+	public function store(RegistrationRequest $request){
+		// Create user
+		$user = User::create([
+			'name' => $request['name'],
+			'email' => $request['email'],
+			'password' => bcrypt($request['password'])
+		]);
 
-    	// Create user
-    	$user = User::create([
-            'name' => request('name'),
-            'email' => request('email'),
-            'password' => bcrypt(request('password'))
-        ]);
+		// Authenticate user
+		auth()->login($user);
 
-    	// Authenticate user
-    	auth()->login($user);
+		// Mail user with welcome
+		\Mail::to($user)->send(new Welcome($user));
 
-        // Mail user with welcome
-        \Mail::to($user)->send(new Welcome($user));
-
-    	//Redirect to the homepage
-    	return redirect()->home();
-    }
+		//Redirect to the homepage
+		return redirect()->home();
+	}
 }
--- Laravel-Learning/first/resources/views/emails/Welcome.blade.php
+++ Laravel-Learning/first/resources/views/emails/Welcome.blade.php
@@ -1,19 +1,14 @@
 @component('mail::message')
 # Introduction
 
-This is the body of my <b>message</b>!
-
-- some
-- not useful
-- points
-- that can eventually
-- be useful
+Welcome to My first Laravel app, {{ $user->name }}!
 
 @component('mail::button', ['url' => 'http://localhost:8000'])
 Start Browsing!
 @endcomponent
 
 @component('mail::panel')
+Don't forget, {{ $user->name }} <br>
 We can put some effect sentence here!
 @endcomponent
 
